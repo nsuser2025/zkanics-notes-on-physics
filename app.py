55fde78cd74df14fe1b8b401ce9bf9aba223ccd7
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-08 07:57:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ markdown_contents = {}
 note_paths = {
     "topics0001": "notes/topics0001.md",
     "topics0002": "notes/topics0002.md",
+    "topics0003": "notes/topics0003.md",
 }
 
 for key, path in note_paths.items():
