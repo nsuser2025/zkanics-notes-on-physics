50bb6c5fd232c940ad02bf99de7e588f3ce71cce
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2026-01-15 08:49:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ note_paths = {
     "mlip_install": "MLIP/install.md",
     "deepmd_dplr": "DEEPMD/dplr.md",
     "aenet_install": "AENET/aenet_install.md",
+    "aenet_lammps": "AENET/aenet_lammps.md",
     "reaxff_install": "LAMMPS/reaxff_install.md",
     "fix_bond_react": "LAMMPS/fix_bond_react.md",
     "fix_bond_react_key": "LAMMPS/fix_bond_react_key.md",
